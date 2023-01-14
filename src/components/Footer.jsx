24cd5abbc65fe8e843e728028730fa37035f984d
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-01-13 23:05:01 -0300

    cta section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import styles from '../style'
+import { logo } from '../assets'
+import { footerLinks, socialMedia } from '../constants'
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => (
+  <div>
+    
+  </div>
+)
 
 export default Footer
